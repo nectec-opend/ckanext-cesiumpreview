ea682d091d2d375f2a9725dc654a90bfd42d4b16
MR KICK ME <79897674+mrkickme@users.noreply.github.com>
2022-11-24 07:08:15 +0700

    Update preview_cesium.js

--- ckanext/cesiumpreview/theme/public/preview_cesium.js
+++ ckanext/cesiumpreview/theme/public/preview_cesium.js
@@ -7,34 +7,9 @@ ckan.module('cesiumpreview', function (jQuery, _) {
 //      var vis_server = 'http://localhost';  //local
 //      var vis_server = 'http://nationalmap.nicta.com.au/';
 	var vis_server = 'https://vmcatalog.nso.go.th/';
-	
-	var spatial ='';
-        var preload_resource='';
-        var extent='';
 		
             var config = {
-                "version": "0.0.03",
-		
-		"initializationUrls":[{
-                    "catalog":[
-                        {
-                            "type":"group",
-                            "name":"Data Catalogue",
-                            "description":"test data set on left button",
-                            "members":[
-                                {
-                                    "type": "ersi-mapServer",
-                                    "name": "Data Catalogue",
-                                    "isUserSupplied": true,
-                                    "isOpen": true,
-                                    "isEnabled": true,
-                                    "url": "https://gistdaportal.gistda.or.th/data/rest/services/GWater/Gistda_water_flood_site/MapServer/9"
-                                }
-                            ]
-                        }
-                    ]
-                }],
-		    
+                "version": "0.0.03",  
                 "initSources": [
 		     {
                     	"catalog": [
@@ -101,28 +76,6 @@ ckan.module('cesiumpreview', function (jQuery, _) {
                     config["initSources"][0]['homeCamera']['north'] = extent[3];
                 }
             }
-		
-// test code
-            config["initializationUrls"][0]['catalog'][0]['members'][0]['url'] = preload_resource['url'];
-            if (preload_resource['url'].indexOf('https') !== 0) {
-                config["initializationUrls"][0]['catalog'][0]['members'][0]['url'] = "https:" + preload_resource['url'];
-            }
-            config["initializationUrls"][0]['catalog'][0]['members'][0]['type'] = preload_resource['format'].toLowerCase();
-
-            if (config["initializationUrls"][0]['catalog'][0]['members'][0]['type'] == 'wms' || config["initializationUrls"][0]['catalog'][0]['members'][0]['type'] == 'wfs') {
-                // if wms_layer specified in resource, display that layer/layers by default
-                if (typeof preload_resource['wms_layer'] != 'undefined' && preload_resource['wms_layer'] != '') {
-                    config["initializationUrls"][0]['catalog'][0]['members'][0]['layers'] = preload_resource['wms_layer'];
-                } else {
-                    config["initializationUrls"][0]['catalog'][0]['members'][0]['type'] = config["initializationUrls"][0]['catalog'][0]['members'][0]['type'] + '-getCapabilities';
-                }
-            }
-            if (config["initializationUrls"][0]['catalog'][0]['members'][0]['type'] == 'arcgis rest api') {
-                config["initializationUrls"][0]['catalog'][0]['members'][0]['type'] = 'esri-mapServer-group';
-            }
-
-// original code
-
             config["initSources"][0]['catalog'][0]['items'][0]['url'] = preload_resource['url'];
                 if (preload_resource['url'].indexOf('http') !== 0) {
                     config["initSources"][0]['catalog'][0]['items'][0]['url'] = "http:" + preload_resource['url'];
